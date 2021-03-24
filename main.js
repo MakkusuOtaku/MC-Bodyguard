f8092a82fc6b3759bd68c757d606f222ac4c4f3b
javaveryhot <49813441+javaveryhot@users.noreply.github.com>
2021-03-24 13:52:37 +0100

    Added comments
    Descriptive comments so people can more easily understand the code

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ var bots = [];
 var target;
 
 function createBot() {
+	//Log in as a new bot to the server
 	let bot = mineflayer.createBot({
 		host: server.address,
 		username: `${prefix}_${bots.length}`,
@@ -27,32 +28,43 @@ function createBot() {
 	bot.id = bots.length;
 	bot.direction = Math.PI*2/botCount*bots.length;
 
+	//Log errors and kick messages
 	bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn));
 	bot.on('error', err => console.log(err));
 
+	//Do this every time the bot moves
 	bot.on('move', ()=>{
 		let boss = bot.players[bossName];
+		//Abort if the boss is not on the server
 		if (!boss) return;
 		boss = boss.entity;
+		//Abort if the boss is not close
 		if (!boss) return;
 
 		offset = boss.yaw;
+		//Location is where the bot is supposed to be headed
 		let location;
-
+		
 		if (target) {
+			//If there is a target (enemy) to attack, make them the target location
 			location = target.position;
 		} else {
+			//If there is no enemy (no combat), return to or keep staying with boss
 			let x = Math.sin(bot.direction+offset)*personalSpace;
 			let z = Math.cos(bot.direction+offset)*personalSpace;
+			//Set the headed location to your position next to boss
 			location = boss.position.offset(x, 0, z);
 		}
-
+		//Face the location it is heading
 		bot.lookAt(location);
+		//If in combat, hit the enemy
 		if (target) bot.attack(target);
-
+		//If it is not yet the amount of blocks "personalSpace" away from the location, walk
 		if (bot.entity.position.xzDistanceTo(location) > personalSpace) {
+			//Sprint forward
 			bot.setControlState('forward', true);
 			bot.setControlState('sprint', true);
+			//Jump in case it is stuck
 			bot.setControlState('jump', bot.entity.isCollidedHorizontally);
 		}
 	});
@@ -60,12 +72,15 @@ function createBot() {
 }
 
 function mainChat(username, message) {
+	//Ignore messages that are not from the boss
 	if (username != bossName) return;
+	//Boss's command parts as an array, e.g. ["kill, "jeb_"]
 	let tokens = message.split(' ');
 
 	switch(tokens[0]) {
 		case 'kill':
 			bots[0].chat("Received.");
+			//Set the target to the nearest entity that has the username by what the boss said
 			target = bots[0].nearestEntity((entity)=>{
 				return(entity.displayName == tokens[1] || entity.username == tokens[1]);
 			});
@@ -78,19 +93,26 @@ function mainChat(username, message) {
 }
 
 function populate() {
+	//Spawn 1 Guard
 	bots.push(createBot());
 	if (bots.length < botCount) {
+		//Wait 10 seconds before doing this once again (10 seconds between each Guard spawn)
 		setTimeout(populate, 10000);
 	} else {
+		//Do this when all bots are spawned
 		console.log("Ready!");
 		bots[0].on('chat', mainChat);
+		//When an entity disappears (quits, dies etc.), and it's the target, remove the target
 		bots[0].on('entityGone', (entity)=>{
 			if (entity != target) return;
 			target = 0;
 		});
+		//When an entity gets harmed
 		bots[0].on('entityHurt', (entity)=>{
+			//Ignore if the harmed entity is not the boss
 			if (entity.username != bossName) return;
 
+			//Select the entity that hurt the boss, by selecting the nearest entity that is not the boss or another Guard
 			//This needs work :/
 			target = bots[0].nearestEntity((entity)=>{
 				return(entity.username != bossName && !(entity.username||'').startsWith(prefix));
@@ -99,4 +121,5 @@ function populate() {
 	}
 }
 
+//Begin the Guard populating!
 populate();
